42cdc15f88c39e2a68c46b33805cd450edc3450b
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-05-10 00:28:59 +0800

    Sub-tasks are written in a new simple-calculator.py file. Proceeding to accomplish sub-tasks one-by-one

---
+++ simple-calculator.py
@@ -0,0 +1,13 @@
+#Ask the user which operations will be used.
+
+
+#Ask the user to enter two (2) numbers.
+
+#Display Result
+
+#Ask the user if they want to repeat the process
+
+#If 'yes', return from step 1
+#If 'no', Display "Thank you!!!", then close the program.
+
+#ps. apply file exception functions.
