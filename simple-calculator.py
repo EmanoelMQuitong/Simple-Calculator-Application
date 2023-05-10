253db387001f899be33e950ca76bfd162a45a639
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-05-10 12:47:47 +0800

    First step process has been added. Boolean operation has been declared. The user will be able to choose operation to be used.

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,8 +1,31 @@
 #Ask the user which operations will be used.
-
+operation = input("Which operation ('addition', 'subtraction', 'multiplication' or 'division') would you like to use? ")
+operation.lower()
 
 #Ask the user to enter two (2) numbers.
+if operation == 'addition':
+    input1 = input("Enter your first number: ")
+    input2 = input("Enter your second number: ")
+    result = int(input1) + int(input2)
+    print(result)
 
+elif operation == 'subtraction':
+    input1 = input("Enter your first number: ")
+    input2 = input("Enter your second number: ")
+    result = int(input1) - int(input2)
+    print(result)
+
+elif operation == 'multiplication':
+    input1 = input("Enter your first number: ")
+    input2 = input("Enter your second number: ")
+    result = int(input1) * int(input2)
+    print(result)
+
+if operation == 'division':
+    input1 = input("Enter your first number: ")
+    input2 = input("Enter your second number: ")
+    result = int(input1) // int(input2)
+    print(result)
 #Display Result
 
 #Ask the user if they want to repeat the process
