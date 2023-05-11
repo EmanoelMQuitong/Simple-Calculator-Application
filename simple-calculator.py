c5e99d479de593f9683c1c5b2aff8c65c33aeea3
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-05-12 00:32:55 +0800

    A simple while loop has been added for the function repetition. The process will repeat as long as the response of the user is 'yes'

--- simple-calculator.py
+++ simple-calculator.py
@@ -3,6 +3,7 @@ operation = input("Which operation ('addition', 'subtraction', 'multiplication' 
 operation.lower()
 
 #Ask the user to enter two (2) numbers.
+
 if operation == 'addition':
     input1 = input("Enter your first number: ")
     input2 = input("Enter your second number: ")
@@ -26,11 +27,47 @@ if operation == 'division':
     input2 = input("Enter your second number: ")
     result = int(input1) // int(input2)
     print(result)
-#Display Result
 
+response = input("Would you like to repeat the process? Type'yes' to proceed, enter 'no' if not.")
+response.lower()
 #Ask the user if they want to repeat the process
+while response.lower() == 'yes':
+    operation = input("Which operation ('addition', 'subtraction', 'multiplication' or 'division') would you like to use? ")
+    operation.lower()
 
-#If 'yes', return from step 1
-#If 'no', Display "Thank you!!!", then close the program.
+    if operation == 'addition':
+        input1 = input("Enter your first number: ")
+        input2 = input("Enter your second number: ")
+        result = int(input1) + int(input2)
+        print(result)
+
+    elif operation == 'subtraction':
+        input1 = input("Enter your first number: ")
+        input2 = input("Enter your second number: ")
+        result = int(input1) - int(input2)
+        print(result)
+
+    elif operation == 'multiplication':
+        input1 = input("Enter your first number: ")
+        input2 = input("Enter your second number: ")
+        result = int(input1) * int(input2)
+        print(result)
+
+    if operation == 'division':
+        input1 = input("Enter your first number: ")
+        input2 = input("Enter your second number: ")
+        result = int(input1) // int(input2)
+        print(result)    
+    
+    response = input("Would you like to repeat the process? Type'yes' to proceed, enter 'no' if not.")
+    response.lower()
+    
+    if response.lower() == 'no':
+        print("End program")
+        
+if response.lower() == 'no':
+    print("End program")
+
+
 
 #ps. apply file exception functions.
