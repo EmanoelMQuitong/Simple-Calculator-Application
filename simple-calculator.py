cbdde4e7618be95831335b4cb0dcbb00351b519e
EmanoelMQuitong <emanoelquitong23@gmail.com>
2023-05-12 08:40:20 +0800

    Colors codes has been added, progress to file exception application has been applied. Operation response will report error if the user entered a non-string and characters that doesn't available from the choices.

--- simple-calculator.py
+++ simple-calculator.py
@@ -1,67 +1,80 @@
+BLUE = '\033[94m'
+DARKCYAN = '\033[36m'
+PURPLE = '\033[95m'
+GREEN = '\033[92m'
+BOLD = '\033[1m'
+END = '\033[0m'
+
 #Ask the user which operations will be used.
-operation = input("Which operation ('addition', 'subtraction', 'multiplication' or 'division') would you like to use? ")
-operation.lower()
+try:
+    operation = input("Which operation ('addition', 'subtraction', 'multiplication' or 'division') would you like to use? ")
+except ValueError:
+    print("Sorry! Only strings are allowed")
+    
+if operation.lower() != 'addition' and operation.lower() != 'subtraction' and operation.lower() != 'multiplication' and operation.lower() != 'division':
+    raise Exception("Please only enter the qouted words among the choices.")
 
 #Ask the user to enter two (2) numbers.
 
-if operation == 'addition':
+if operation.lower() == 'addition':
     input1 = input("Enter your first number: ")
     input2 = input("Enter your second number: ")
-    result = int(input1) + int(input2)
+    result = float(input1) + float(input2)
     print(result)
 
-elif operation == 'subtraction':
+elif operation.lower() == 'subtraction':
     input1 = input("Enter your first number: ")
     input2 = input("Enter your second number: ")
-    result = int(input1) - int(input2)
+    result = float(input1) - float(input2)
     print(result)
 
-elif operation == 'multiplication':
+elif operation.lower() == 'multiplication':
     input1 = input("Enter your first number: ")
     input2 = input("Enter your second number: ")
-    result = int(input1) * int(input2)
+    result = float(input1) * int(input2)
     print(result)
 
-if operation == 'division':
+if operation.lower() == 'division':
     input1 = input("Enter your first number: ")
     input2 = input("Enter your second number: ")
-    result = int(input1) // int(input2)
+    result = float(input1) // float(input2)
     print(result)
 
 response = input("Would you like to repeat the process? Type'yes' to proceed, enter 'no' if not.")
 response.lower()
+
 #Ask the user if they want to repeat the process
 while response.lower() == 'yes':
     operation = input("Which operation ('addition', 'subtraction', 'multiplication' or 'division') would you like to use? ")
     operation.lower()
 
-    if operation == 'addition':
+    if operation.lower() == 'addition':
         input1 = input("Enter your first number: ")
         input2 = input("Enter your second number: ")
-        result = int(input1) + int(input2)
+        result = float(input1) + float(input2)
         print(result)
 
-    elif operation == 'subtraction':
+    elif operation.lower() == 'subtraction':
         input1 = input("Enter your first number: ")
         input2 = input("Enter your second number: ")
-        result = int(input1) - int(input2)
+        result = float(input1) - float(input2)
         print(result)
 
-    elif operation == 'multiplication':
+    elif operation.lower() == 'multiplication':
         input1 = input("Enter your first number: ")
         input2 = input("Enter your second number: ")
-        result = int(input1) * int(input2)
+        result = float(input1) * float(input2)
         print(result)
 
-    if operation == 'division':
+    if operation.lower() == 'division':
         input1 = input("Enter your first number: ")
         input2 = input("Enter your second number: ")
-        result = int(input1) // int(input2)
+        result = float(input1) / float(input2)
         print(result)    
     
     response = input("Would you like to repeat the process? Type'yes' to proceed, enter 'no' if not.")
     response.lower()
-    
+
     if response.lower() == 'no':
         print("End program")
         
